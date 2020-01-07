19195a2e64331393d9d51e9a23992ca19c8c7096
Claude Zou <claude.zou.work@gmail.com>
2020-01-07 09:46:18 -0500

    Add possible enemy locs

--- bot1/HQ.java
+++ bot1/HQ.java
@@ -7,6 +7,22 @@ import static bot1.RobotPlayer.rc;
 
 public class HQ {
   static void runHQ() throws GameActionException {
+    int width = rc.getMapWidth();
+    int height = rc.getMapHeight();
+    MapLocation middle = new MapLocation(width / 2, height / 2);
+    MapLocation myLoc = rc.getLocation();
+    MapLocation[] possible_enemy_locs = new MapLocation[3];
+
+    // set locations
+    int delta_x = middle.x + (middle.x - myLoc.x);
+    int delta_y = middle.y + (middle.y - myLoc.y);
+    possible_enemy_locs[0] = new MapLocation(delta_x, delta_y);
+    possible_enemy_locs[1] = new MapLocation(delta_x, myLoc.y);
+    possible_enemy_locs[2] = new MapLocation(myLoc.x, delta_y);
+    /*System.out.println(possible_enemy_locs[0]);
+    System.out.println(possible_enemy_locs[1]);
+    System.out.println(possible_enemy_locs[2]);*/
+
     for (Direction dir : Helper.directions)
       Helper.tryBuild(RobotType.MINER, dir);
   }
