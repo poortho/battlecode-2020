0d5814de1aa7315a30a6f9fca5ecf78bd6f4bfb3
Alex Lin <lin969@purdue.edu>
2020-01-11 21:25:04 -0500

    no more 3 drones at hq

--- bot5/FulfillmentCenter.java
+++ bot5/FulfillmentCenter.java
@@ -55,7 +55,6 @@ public class FulfillmentCenter {
         // scale threshold based on number of drones nearby
         if (enemy_hq == 0 && enemy_net_gun == 0) {
             if ((num_enemy_units > 0 && rc.getTeamSoup() >= RobotType.DELIVERY_DRONE.cost*(1+num_drones)) ||
-                    (near_hq && (num_drones < num_enemy_units || drones_produced < 3) && rc.getTeamSoup() >= RobotType.DELIVERY_DRONE.cost) ||
                     (rc.getTeamSoup() >= 450 && near_hq) ||
                     (Helper.getLevel(round) + 1 == rc.senseElevation(cur_loc) && near_hq)) {
                 Helper.tryBuild(RobotType.DELIVERY_DRONE);
