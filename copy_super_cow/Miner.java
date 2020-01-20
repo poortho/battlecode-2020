e41b6b4c66d51eb71d4ca422a65a96f1f7fa28cd
Alex Lin <lin969@purdue.edu>
2020-01-19 22:55:18 -0500

    dont build duplicate design schools

--- copy_super_cow/Miner.java
+++ copy_super_cow/Miner.java
@@ -790,7 +790,7 @@ public class Miner {
   }
 
   static RobotType calcBuilding() {
-  	if (near_hq && !nearby_design && gay_rush_alert) {
+  	if (near_hq && !nearby_design && gay_rush_alert && first_miner) {
   		return RobotType.DESIGN_SCHOOL;
   	} else if (num_enemy_drones >= 1 && !nearby_netgun) {
   		return RobotType.NET_GUN;
